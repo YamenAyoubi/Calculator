675e1e05c829fea35862a193024f2f98e3c84c94
YamenAyoubi <YamenAyoubi@users.noreply.github.com>
2018-12-12 09:46:01 +0100

    enter

--- src/main/java/Eclipse/Calculator/App.java
+++ src/main/java/Eclipse/Calculator/App.java
@@ -8,8 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-       for(int i =1; i>=10; i++) {
-    	   System.out.println(i);
+       for(int a =1; a>=10; a++) {
+    	   System.out.println(a);
     }
 }
 }
