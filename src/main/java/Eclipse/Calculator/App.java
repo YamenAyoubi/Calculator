35b4b0ffe0775592ff31d91fc29f1768e30ea44c
YamenAyoubi <YamenAyoubi@users.noreply.github.com>
2018-12-12 09:44:19 +0100

    initial commit

--- src/main/java/Eclipse/Calculator/App.java
+++ src/main/java/Eclipse/Calculator/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       for(int i =1; i>=10; i++) {
+    	   System.out.println(i);
     }
+}
 }
