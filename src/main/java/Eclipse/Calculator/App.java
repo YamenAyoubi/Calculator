06e7d1e065f03f6bdf6c17006493b6a81886b42c
YamenAyoubi <YamenAyoubi@users.noreply.github.com>
2018-12-12 10:11:03 +0100

    enter

--- src/main/java/Eclipse/Calculator/App.java
+++ src/main/java/Eclipse/Calculator/App.java
@@ -1,5 +1,7 @@
 package Eclipse.Calculator;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,8 +10,28 @@ public class App
 {
     public static void main( String[] args )
     {
-       for(int a =1; a<=10; a++) {
-    	   System.out.println(a);
+    	  Scanner in = new Scanner(System.in);
+    	   
+    	  System.out.print("Input first number: ");
+    	  int num1 = in.nextInt();
+    	   
+    	  System.out.print("Input second number: ");
+    	  int num2 = in.nextInt();
+    	   
+    	 
+    	  System.out.println(num1 + " + " + num2 + " = " + 
+    	  (num1 + num2));
+    	   
+    	  System.out.println(num1 + " - " + num2 + " = " + 
+    	  (num1 - num2));
+    	   
+    	  System.out.println(num1 + " x " + num2 + " = " + 
+    	  (num1 * num2));
+    	   
+    	  System.out.println(num1 + " / " + num2 + " = " + 
+    	  (num1 / num2));
+    	 
+    	  System.out.println(num1 + " ^ " + num2 + " = " + 
+    	  (num1 % num2));
     }
-}
 }
